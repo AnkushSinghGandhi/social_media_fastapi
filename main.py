d1e9f9687b9a4ef82efdd1d622b861de9fa502ad
Ankush Singh Gandhi <ankushsinghgandhi@gmail.com>
2024-10-03 22:37:28 +0530

    added user registration route

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status
+from schema import UserCreate
+from utils import hash_password
 
 app = FastAPI()
 
+fake_users_db = {}
 
-@app.get("/")
-def root():
-    return{"message": "Welcome to our Social Media App!"}
+@app.post("/register")
+def register_user(user: UserCreate):
+    if user.email in fake_users_db:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Email already registered"
+        )
+    
+    hashed_password = hash_password(user.password)
+    fake_users_db[user.email] = {
+        "username": user.username,
+        "email": user.email,
+        "password": hashed_password
+    }
+    return {"message": "User registered successfully"}
