ec9465ae98f449153c7384cce24ef03fd8dc3181
Ankush Singh Gandhi <ankushsinghgandhi@gmail.com>
2024-10-21 17:15:27 +0530

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://ankushsinghgandhi:password@localhost/social_media_app"
+DATABASE_URL = os.getenv('DATABASE_URL')
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
